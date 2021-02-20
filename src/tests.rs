0899244e1ee5ba573042296cbf24db21d448f903
Muhammad-Jibril <jbashir52@gmail.com>
2021-02-21 02:56:02 +0800

    Fix comments

--- src/tests.rs
+++ src/tests.rs
@@ -6,8 +6,8 @@ use frame_support::{assert_ok, assert_noop};
 #[test]
 fn set_price_works() {
 	new_test_ext().execute_with(|| {
-		// Just a dummy test for the dummy function `do_something`
-		// calling the `do_something` function with a value 42
+		// Just a set_price test for the `set_price` function `
+		// calling the `set_price` function with a value 42
 		assert_ok!(PriceModule::set_price(Origin::signed(1), 42));
 		// asserting that the stored value is equal to what we stored
 		assert_eq!(PriceModule::get_price(), 42);
