0bacdfea2c27335a2a5312be2425c954487e9344
Muhammad-Jibril <jbashir52@gmail.com>
2021-03-14 10:12:01 +0800

    fixes again

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
+//! A simple price feed pallet
 #![cfg_attr(not(feature = "std"), no_std)]
+#![allow(clippy::unused_unit)]
 
-/// A price feed pallet
-use frame_support::{debug::native, decl_error, decl_event, decl_module, decl_storage, dispatch};
+use sp_std::prelude::*;
+
+use codec::{Decode, Encode};
+use frame_support::pallet_prelude::*;
 use sp_runtime::{
-	traits::{AtLeast32Bit, CheckedAdd, CheckedDiv, CheckedMul, MaybeSerializeDeserialize, Member, Zero}, 
-	DispatchResult, PerThing, Perbill, RuntimeDebug,
+	traits::{
+		AccountIdConversion, AtLeast32BitUnsigned, Bounded, CheckedAdd, CheckedSub, MaybeSerializeDeserialize, Member,
+		Saturating, StaticLookup, Zero,
+	},
+	DispatchError, DispatchResult, ModuleId, RuntimeDebug,
 };
 use frame_system::{self as system, ensure_signed, pallet_prelude::*};
 use stp258_traits::{Stp258Currency};
@@ -12,61 +19,75 @@ use stp258_traits::{Stp258Currency};
 mod mock;
 mod tests;
 
-impl<T: Trait> FetchPrice<u32> for Module<T> {
-    fn fetch_price() -> u32 {
-        Self::get_price()
+pub use module::*;
+
+#[frame_support::pallet]
+pub mod module {
+	use super::*;
+
+    #[pallet::pallet]
+	pub struct Pallet<T>(PhantomData<T>);
+
+    /// The pallet's configuration trait.
+   #[pallet::config]
+	pub trait Config: frame_system::Config {
+        // Add other types and constants required to configure this pallet.
+
+        /// The overarching event type.
+       type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        
+        /// The currency ID type
+        type CurrencyId: Parameter + Member + Copy + MaybeSerializeDeserialize + Ord;
     }
-}
 
-/// The pallet's configuration trait.
-pub trait Trait: frame_system::Trait {
-    // Add other types and constants required to configure this pallet.
+    // This pallet's storage items.
+    /// The Price of a currency type.
+    #[pallet::storage]
+    #[pallet::getter(fn price)]
+    pub type Price<T: Config> = StorageDoubleMap<
+        _,
+        Twox64Concat,
+        T::CurrencyId,
+        Twox64Concat,
+        T::Balance,
+        ValueQuery,
+    >;
 
-    /// The overarching event type.
-    type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
-
-    /// The currency ID type
-	type CurrencyId: Parameter + Member + Copy + MaybeSerializeDeserialize + Ord;
-}
-
-// This pallet's storage items.
-decl_storage! {
-    trait Store for Module<T: Trait> as Price {
-        Price get(fn get_price): u32 = 1_000;
-    }
-}
-
-// The pallet's events
-decl_event!(
-    pub enum Event<T>
-    {
+    // The pallet's events
+    #[pallet::event]
+    #[pallet::generate_deposit(fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// The New Price of Currency. [currency_id, price]
         NewPrice(u32),
-
     }
-);
 
-// The pallet's errors
-decl_error! {
-    pub enum Error for Module<T: Trait> {
-        NoOffchainPrice
+
+    // The pallet's errors
+    #[pallet::error]
+    pub enum Error<T> {
+        /// Some wrong behavior
+        Wrong,
+        /// Something went very wrong and the price of the currency is zero.
+        ZeroPrice,
+        /// No offchain price available.
+        NoOffchainPrice,
+        /// While trying to contract the supply, it underflowed.
+        SupplyUnderflow,
     }
-}
 
-// The pallet's dispatchable functions.
-decl_module! {
-    /// The module declaration.
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        type Error = Error<T>;
+	#[pallet::hooks]
+	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {}
+    
+    #[pallet::call]
+	impl<T: Config> Pallet<T> {
 
-        fn deposit_event() = default;
-
-        #[weight = 0]
-        pub fn set_price(origin, currency_id: CurrencyId, new_price: u32) -> dispatch::DispatchResult {
+        #[pallet::weight(0)]
+        pub fn set_price(origin: OriginFor<T>, currency_id: T::CurrencyId, new_price: u32) -> DispatchResultWithPostInfo {
             let _who = ensure_signed(origin)?;
 
             Price::put(currency_id, new_price);
 
-            Self::deposit_event(RawEvent::NewPrice(currency_id, new_price));
+            Self::deposit_event(Event::NewPrice(currency_id, new_price));
 
             Ok(())
         }
